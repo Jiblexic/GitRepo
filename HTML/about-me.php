f3353d2dddef9e3f0f07b3a9ff5a72f9813a00d3
Jiblexic <dan-regan@outlook.com>
2015-09-30 16:52:19 +0100

    Changes!

---
+++ HTML/about-me.php
@@ -0,0 +1,129 @@
+<?php 
+	$pageTitle="About Me"; 
+	include_once( "main_header.php"); 
+?>
+
+			<div role="main" class="main">
+				<div id="spacers" style="margin-top: 20px">&nbsp;</div>
+				<div class="container">
+					<div class="row">
+						<div class="col-md-4">
+							<div class="owl-carousel" data-plugin-options='{"items": 1, "margin": 10}'>
+								<div>
+									<span class="img-thumbnail">
+										<img alt="" height="300" class="img-responsive" src="img/players/dan_hoyland.jpg">
+									</span>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-8">
+
+							<h2 class="mb-none">Dan <strong>Hoyland</strong></h2>
+							<h4 class="heading-primary">Striker</h4>
+
+							<hr class="solid">
+
+							<span class="social-icons-list">
+								<a data-tooltip data-placement="bottom" target="_blank" href="http://www.facebook.com" data-original-title="Facebook"><i class="fa fa-facebook"></i><span>Facebook</span></a>
+								<a data-tooltip data-placement="bottom" href="http://www.twitter.com" data-original-title="Twitter"><i class="fa fa-twitter"></i><span>Twitter</span></a>
+							</span>
+
+							<p>Dan Hoyland learnt his trade on the mean streets of Exeter where he found his place at Centre Back, forming partnerships with ease and having strikers in his back pocket Dan was ahead of the curve. It wasn't until later in his career did Dan make the Dion Dublin style change from defense to attack did things really take off for him.
+
+							<br>
+							<br>
+							Scoring week in, week out, giving goalkeepers nightmares Dan went from strength to strength up-front, as Cheriton Fitzpaines top goalscorer for the 2014/2015 campaign he hopes to have his name firmly on the new gaffers teamsheet.</p>
+
+
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-12">
+							<hr>
+							<h2 class="panel-title" style="margin-bottom: -10px; text-align: center"><strong>Stats</strong> </h2>
+						</div>
+					</div>
+					<div class="row mt-xlg">
+						<div class="col-md-4">
+							<div class="circular-bar">
+								<div class="circular-bar-chart" data-percent="100" data-plugin-options='{"barColor": "#E36159"}'>
+									<strong>6</strong>
+									<label><span>Played</span></label>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="circular-bar">
+								<div class="circular-bar-chart" data-percent="110" data-plugin-options='{"barColor": "#0088CC"}'>
+									<strong>8</strong>
+									<label><span>Goals</span></label>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="circular-bar">
+								<div class="circular-bar-chart" data-percent="100" data-plugin-options='{"barColor": "#2BAAB1"}'>
+									<strong>1</strong>
+									<label><span >Assists</span></label>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+
+
+				
+			</div>
+
+			<?php include_once( "footer.php"); ?>
+
+		<!-- Vendor -->
+		<!--[if lt IE 9]>
+		<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+		<![endif]-->
+		<!--[if gte IE 9]><!-->
+		<script src="vendor/jquery/jquery.js"></script>
+		<!--<![endif]-->
+		<script src="vendor/jquery.appear/jquery.appear.js"></script>
+		<script src="vendor/jquery.easing/jquery.easing.js"></script>
+		<script src="vendor/jquery-cookie/jquery-cookie.js"></script>
+		<script src="vendor/bootstrap/bootstrap.js"></script>
+		<script src="vendor/common/common.js"></script>
+		<script src="vendor/jquery.validation/jquery.validation.js"></script>
+		<script src="vendor/jquery.stellar/jquery.stellar.js"></script>
+		<script src="vendor/jquery.easy-pie-chart/jquery.easy-pie-chart.js"></script>
+		<script src="vendor/jquery.gmap/jquery.gmap.js"></script>
+		<script src="vendor/isotope/jquery.isotope.js"></script>
+		<script src="vendor/owlcarousel/owl.carousel.js"></script>
+		<script src="vendor/jflickrfeed/jflickrfeed.js"></script>
+		<script src="vendor/magnific-popup/jquery.magnific-popup.js"></script>
+		<script src="vendor/vide/vide.js"></script>
+		
+		<!-- Theme Base, Components and Settings -->
+		<script src="js/theme.js"></script>
+		
+		<!-- Theme Custom -->
+		<script src="js/custom.js"></script>
+		
+		<!-- Theme Initialization Files -->
+		<script src="js/theme.init.js"></script>
+
+		<!-- Google Analytics: Change UA-XXXXX-X to be your site's ID. Go to http://www.google.com/analytics/ for more information.
+		<script type="text/javascript">
+		
+			var _gaq = _gaq || [];
+			_gaq.push(['_setAccount', 'UA-12345678-1']);
+			_gaq.push(['_trackPageview']);
+		
+			(function() {
+			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+			})();
+		
+		</script>
+		 -->
+
+	</body>
+</html>
